2e080e18b247987e50aec81f5ea7b84b43bfe504
seoye0ng <bsy0313@gmail.com>
2023-06-04 18:25:26 +0900

    Feat: 상세페이지로 이동시 스크롤 최상단으로 오도록 ScrollTop 컴포넌트 구현 (#1)

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollTop from '../components/common/ScrollTop';
 import Header from '../components/common/Header/Header';
 import ProductInfo from '../components/productDetail/ProductInfo';
 import Footer from '../components/common/Footer/Footer';
@@ -6,6 +7,7 @@ import Footer from '../components/common/Footer/Footer';
 export default function ProductDetails() {
   return (
     <>
+      <ScrollTop />
       <Header />
       <ProductInfo />
       <Footer />
