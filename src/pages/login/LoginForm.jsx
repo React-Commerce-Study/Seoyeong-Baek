5b15689bca5fe073327a11e23b00879f5fdb8757
seoye0ng <bsy0313@gmail.com>
2023-06-12 00:30:09 +0900

    Feat: 로그인 input별 예외처리 (#3)

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -33,8 +33,21 @@ export default function LoginForm({ loginType }) {
       }
     } catch {
       console.error('Login failed!');
+      setIsLoginError(true);
     }
   }
+
+  const [isUsernameError, setIsUsernameError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [isLoginError, setIsLoginError] = useState(false);
+
+  const handleError = () => {
+    if ((loginData.username === '' && loginData.password === '') || loginData.username === '') {
+      setIsUsernameError(true);
+    } else if (loginData.username && loginData.password === '') {
+      setIsPasswordError(true);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -52,6 +65,7 @@ export default function LoginForm({ loginType }) {
             value={loginData.username}
             onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
           />
+          <MessageError display={isUsernameError}>아이디를 입력해 주세요.</MessageError>
         </label>
 
         <label htmlFor="pw">
@@ -62,10 +76,14 @@ export default function LoginForm({ loginType }) {
             value={loginData.password}
             onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
           />
+          <MessageError display={isPasswordError}>비밀번호를 입력해 주세요.</MessageError>
+          <MessageError display={isLoginError}>아이디 또는 비밀번호가 일치하지 않습니다.</MessageError>
         </label>
 
         <div className="btn-box">
-          <Button type="submit">로그인</Button>
+          <Button type="submit" onClick={handleError}>
+            로그인
+          </Button>
         </div>
       </LoginFormStyle>
     </div>
@@ -99,4 +117,11 @@ const LoginFormStyle = styled.form`
     margin-top: 36px;
     /* box-shadow: inset 0 0 10px red; */
   }
+`;
+
+const MessageError = styled.p`
+  color: red;
+  margin-top: 26px;
+  text-align: left;
+  display: ${(props) => (props.display ? 'block' : 'none')};
 `;
