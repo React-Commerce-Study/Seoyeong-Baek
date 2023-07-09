9bf6d60d2963503da5eb0119afd1dc370070f840
seoye0ng <bsy0313@gmail.com>
2023-07-09 23:55:15 +0900

    Feat: 회원가입 api 요청하기 (#4)

--- src/pages/signUp/SignUpForm.jsx
+++ src/pages/signUp/SignUpForm.jsx
@@ -3,112 +3,118 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/common/Buttons/Button';
 import styled from 'styled-components';
 
-export default function SignUpForm({ loginType }) {
+export default function SignUpForm() {
   const navigate = useNavigate();
   const URL = 'https://openmarket.weniv.co.kr/';
-  const reqPath = 'accounts/login/';
+  const reqPath = 'accounts/signup/';
 
-  const [loginData, setLoginData] = useState({
+  const [signUpData, setSignUpData] = useState({
     username: '',
     password: '',
-    login_type: loginType,
+    password2: '',
+    phone_number: '', // 전화번호는 010으로 시작하는 10~11자리 숫자
+    name: '',
   });
+  console.log(signUpData);
 
-  async function handleSignUp(loginData) {
-    // try {
-    //   const res = await fetch(URL + reqPath, {
-    //     method: 'POST',
-    //     headers: { 'Content-type': 'application/json' },
-    //     body: JSON.stringify(loginData),
-    //   });
-    //   if (res.ok) {
-    //     console.log('Login successful!');
-    //     const json = await res.json();
-    //     const token = json.token;
-    //     localStorage.setItem('token', token);
-    //     navigate('/');
-    //   } else {
-    //     throw new Error();
-    //   }
-    // } catch {
-    //   console.error('Login failed!');
-    //   setIsLoginError(true);
-    // }
+  async function handleSignUp(signUpData) {
+    try {
+      const res = await fetch(URL + reqPath, {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(signUpData),
+      });
+      console.log(signUpData);
+
+      if (res.ok) {
+        const json = await res.json();
+        console.log('SignUp successful!');
+        console.log(json);
+        // navigate('/login');
+      } else {
+        throw new Error();
+      }
+    } catch {
+      console.error('signUp failed!');
+      // setIsLoginError(true);
+    }
   }
+
+  //   TODO: 에러메세지 저장해놓기
 
   const [isUsernameError, setIsUsernameError] = useState(false);
   const [isPasswordError, setIsPasswordError] = useState(false);
   const [isLoginError, setIsLoginError] = useState(false);
 
   const handleError = () => {
-    if ((loginData.username === '' && loginData.password === '') || loginData.username === '') {
+    if ((signUpData.username === '' && signUpData.password === '') || signUpData.username === '') {
       setIsUsernameError(true);
-    } else if (loginData.username && loginData.password === '') {
+    } else if (signUpData.username && signUpData.password === '') {
       setIsPasswordError(true);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleSignUp(loginData);
+    handleSignUp(signUpData);
   };
 
   return (
-    <SignUpFormContainer>
-      <SignUpFormStyle action="" onSubmit={handleSubmit}>
+    <SignUpFormContainer action="" onSubmit={handleSubmit}>
+      <SignUpFormStyle>
         <label htmlFor="id">아이디</label>
         <IdContainer>
           <input
             type="text"
             id="id"
             placeholder="아이디"
-            value={loginData.username}
-            onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
+            value={signUpData.username}
+            onChange={(e) => setSignUpData({ ...signUpData, username: e.target.value })}
           />
           <Button type="submit">중복확인</Button>
         </IdContainer>
 
-        <MessageError display={isUsernameError}>이미 사용 중인 아이디입니다.</MessageError>
+        {/* <MessageError display={isUsernameError}>이미 사용 중인 아이디입니다.</MessageError> */}
 
         <label htmlFor="pw">비밀번호</label>
         <input
           type="password"
           id="pw"
           placeholder="비밀번호"
-          value={loginData.password}
-          onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+          value={signUpData.password}
+          onChange={(e) => setSignUpData({ ...signUpData, password: e.target.value })}
         />
-        <MessageError display={isPasswordError}>비밀번호를 입력해 주세요.</MessageError>
+        {/* <MessageError display={isPasswordError}>비밀번호를 입력해 주세요.</MessageError> */}
 
         <label htmlFor="check-pw">비밀번호 재확인</label>
         <input
           type="password"
           id="check-pw"
           placeholder="비밀번호"
-          value={loginData.password}
-          onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+          value={signUpData.password2}
+          onChange={(e) => setSignUpData({ ...signUpData, password2: e.target.value })}
         />
-        <MessageError display={isPasswordError}>비밀번호가 일치하지 않습니다.</MessageError>
+        {/* <MessageError display={isPasswordError}>비밀번호가 일치하지 않습니다.</MessageError> */}
 
         <label htmlFor="name">이름</label>
         <input
           type="text"
           id="name"
-          value={loginData.name}
-          onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
+          value={signUpData.name}
+          onChange={(e) => setSignUpData({ ...signUpData, name: e.target.value })}
         />
 
         <label htmlFor="phone-number">휴대폰 번호</label>
         <input
           type="tel"
           id="phone-number"
-          value={loginData.phone_number}
-          onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
+          value={signUpData.phone_number}
+          onChange={(e) => setSignUpData({ ...signUpData, phone_number: e.target.value })}
         />
       </SignUpFormStyle>
 
       <label htmlFor="agree">
-        <input type="checkbox" id="phone-number" />
+        <input type="checkbox" id="agreer" />
         호두샵의 <strong>이용약관</strong> 및 <strong>개인정보처리방침</strong>에 대한 내용을 확인하였고 동의합니다.
       </label>
       <Button type="submit">가입하기</Button>
