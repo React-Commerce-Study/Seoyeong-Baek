e029bfb20303f6d6ef781251ce4e1a9cf71a5e01
seoye0ng <bsy0313@gmail.com>
2023-07-18 21:13:12 +0900

    Fix: 상세페이지 설명버튼 스타일 컴포넌트 코드 수정

--- src/components/common/Buttons/ProductDescriptionBtn.tsx
+++ src/components/common/Buttons/ProductDescriptionBtn.tsx
@@ -24,7 +24,7 @@ const DescriptionBtnStyle = styled.button`
   font-weight: 500;
   box-shadow: 0 6px 0 0 #e0e0e0;
   font-size: 18px;
-  ${(props: ProductDescriptionBtnProps) => props.className && props.className.includes('active') && activeStyles}
+  ${(props: { className?: string }) => props.className && props.className.includes('active') && activeStyles}
 `;
 /* 위 코드에서 props.className 값이 undefined인 경우를 처리하기 위해 props.className이 존재하는지(props.className &&)를 확인. 그런 다음 props.className.includes('active')를 평가하여 'props.className' is possibly 'undefined' 오류를 방지. */
 
