5b9c2fec1b7056d94019c7a60f71af1da3e9f25e
seoye0ng <bsy0313@gmail.com>
2023-07-24 23:54:41 +0900

    ✨Feat: recoil 설치 및 최상단 감싸기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import GlobalStyle from './GlobalStyle';
+import { RecoilRoot } from 'recoil';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/login/Login';
 import SignUp from './pages/signUp/SignUp';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-      </Routes>
-    </>
+    <RecoilRoot>
+      <SLayout>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </SLayout>
+    </RecoilRoot>
   );
 }
+
+const SLayout = styled.div`
+  padding: 0 1em;
+`;
 
 export default App;
