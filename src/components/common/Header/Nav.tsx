085156812ca41b89aaf2419062344883c0044385
seoye0ng <bsy0313@gmail.com>
2023-08-18 18:12:41 +0900

    ✨Feat: 헤더 컴포넌트 마이페이지 아이콘 모달 구현 (#2)

--- src/components/common/Header/Nav.tsx
+++ src/components/common/Header/Nav.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ShoppingCart from '../../../assets/icon/icon-shopping-cart.svg';
 import ShoppingCartActive from '../../../assets/icon/icon-shopping-cart-2.svg';
 import User from '../../../assets/icon/icon-user.svg';
 import UserActive from '../../../assets/icon/icon-user-2.svg';
-
+import MyPageModal from '../../modal/MyPageModal';
 import styled from 'styled-components';
 
 interface NavProps {
@@ -13,6 +13,12 @@ interface NavProps {
 
 export default function Nav({ page }: NavProps) {
   const token = localStorage.getItem('token');
+
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  const handleModalClick = () => {
+    isShowModal ? setIsShowModal(false) : setIsShowModal(true);
+  };
 
   return (
     <SNavContainer>
@@ -23,15 +29,18 @@ export default function Nav({ page }: NavProps) {
           </Link>
         </li>
         {token ? (
-          <li>
+          <li className="modal-btn">
             <Link to="/">
-              <button className="nav-btn login">마이페이지</button>
+              <button className={`nav-btn login ${isShowModal ? 'active' : ''}`} onClick={handleModalClick}>
+                마이페이지
+              </button>
             </Link>
+            {isShowModal && <MyPageModal />}
           </li>
         ) : (
           <li>
             <Link to="/login">
-              <button className={`nav-btn login`}>로그인</button>
+              <button className="nav-btn login">로그인</button>
             </Link>
           </li>
         )}
@@ -46,6 +55,10 @@ const SNavContainer = styled.div`
     align-items: center;
     justify-content: center;
     gap: 1.625rem;
+
+    li.modal-btn {
+      position: relative;
+    }
 
     .nav-btn {
       color: #767676;
@@ -83,6 +96,12 @@ const SNavContainer = styled.div`
     .login.active::before,
     .login:hover::before {
       background: url(${UserActive}) no-repeat center / contain;
+    }
+
+    .modal-wrapper {
+      /* width: 1920px; */
+      /* height: 100vh; */
+      box-shadow: inset 0 0 10px red;
     }
   }
 `;
---
+++ src/components/modal/MyPageModal.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import Bubble from '../../assets/icon/Union.png';
+
+export default function MyPageModal() {
+  return (
+    <SMyPageModalLayout>
+      <img src={Bubble} alt="말풍선 모달 이미지" />
+      <ul>
+        <li>
+          <button>마이페이지</button>
+        </li>
+        <li>
+          <button>로그아웃</button>
+        </li>
+      </ul>
+    </SMyPageModalLayout>
+  );
+}
+
+const SMyPageModalLayout = styled.article`
+  max-width: 130px;
+  position: absolute;
+  top: 3.5rem;
+  left: 50%;
+  transform: translate(-50%);
+  z-index: 99;
+
+  img {
+    max-width: inherit;
+    /* max-width: 130px; */
+
+    filter: drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.25));
+  }
+
+  ul {
+    position: absolute;
+    top: 20px;
+    width: 100%;
+    box-sizing: border-box;
+    text-align: center;
+
+    button {
+      padding: 10px 0;
+      width: 110px;
+      font-size: 16px;
+      font-weight: 500;
+      color: #767676;
+      transition: all 0.25s ease-out;
+
+      &:first-child {
+        margin-bottom: 8px;
+      }
+
+      &:hover {
+        border-radius: 5px;
+        box-shadow: inset 0 0 0 1px #767676;
+        color: #000;
+      }
+    }
+  }
+`;
