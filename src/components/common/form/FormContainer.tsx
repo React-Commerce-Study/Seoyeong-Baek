a16fe591efd0c072aa1bdcecf73bf7c7f93d1db3
seoye0ng <bsy0313@gmail.com>
2023-09-18 04:59:53 +0900

    ♻️Refactor: 로그인페이지, 회원가입페이지 중복 코드 컴포넌트화 (#3, #4)

---
+++ src/components/common/form/FormContainer.tsx
@@ -0,0 +1,86 @@
+import { useState, ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import SignUpForm from '../../../pages/signUp/SignUpForm';
+import LoginForm from '../../../pages/login/LoginForm';
+import LogoImg from '../../../assets/icon/Logo-hodu.png';
+import styled from 'styled-components';
+
+interface FormContainer {
+  setSuccessUserName?: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function FormContainer({ setSuccessUserName }: FormContainer) {
+  const [userType, setUserType] = useState('BUYER');
+
+  const handleLoginType = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === 'BUYER') {
+      setUserType('BUYER');
+    } else if (e.target.value === 'SELLER') {
+      setUserType('SELLER');
+    }
+  };
+
+  console.log(userType);
+
+  return (
+    <>
+      <SFormContainer>
+        <h1>
+          <Link to="/">
+            <img src={LogoImg} alt="로고이미지" />
+          </Link>
+        </h1>
+
+        <div className="login-type-wrapper">
+          <LoginBtnStyle value="BUYER" onClick={handleLoginType} clickChange={userType === 'BUYER'}>
+            구매회원 회원가입
+          </LoginBtnStyle>
+          <LoginBtnStyle value="SELLER" onClick={handleLoginType} clickChange={userType === 'SELLER'}>
+            판매회원 회원가입
+          </LoginBtnStyle>
+        </div>
+        {setSuccessUserName ? (
+          <SignUpForm signUpType={userType} setSuccessUserName={setSuccessUserName} />
+        ) : (
+          <LoginForm loginType={userType} />
+        )}
+      </SFormContainer>
+    </>
+  );
+}
+
+const SFormContainer = styled.div`
+  margin: 100px auto;
+  max-width: 550px;
+  text-align: center;
+
+  h1 img {
+    max-width: 238px;
+    margin-bottom: 70px;
+  }
+
+  .join-find-wrapper {
+    margin-top: 30px;
+    color: #333;
+    font-weight: 400;
+    font-size: 16px;
+  }
+`;
+
+type ClickChangeType = {
+  clickChange: boolean;
+};
+
+const LoginBtnStyle = styled.button`
+  margin-bottom: -10px;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  width: 275px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border: 1px solid #c4c4c4;
+  border-bottom: ${(props: ClickChangeType) => (props.clickChange ? 'none' : '')};
+  background-color: ${(props: ClickChangeType) => (props.clickChange ? '#fff' : '#F2F2F2')};
+  padding: 20px;
+`;
--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,83 +1,14 @@
-import React, { useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
-import SignUpForm from './SignUpForm';
-import LogoImg from '../../assets/icon/Logo-hodu.png';
-import styled from 'styled-components';
+import { useState } from 'react';
 import Modal from '../../components/modal/Modal';
-
-// import Button from '../../components/common/Buttons/Button';
+import FormContainer from '../../components/common/form/FormContainer';
 
 export default function SignUp() {
-  const [successUserName, setSuccessUserName] = useState('');
-  const [signUpType, setSignUpType] = useState('BUYER');
-
-  const handleLoginType = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value === 'BUYER') {
-      setSignUpType('BUYER');
-    } else if (e.target.value === 'SELLER') {
-      setSignUpType('SELLER');
-    }
-  };
-
-  console.log(signUpType);
+  const [successUserName, setSuccessUserName] = useState<string>('');
 
   return (
     <>
-      <SSignUpContainer>
-        <h1>
-          <Link to="/">
-            <img src={LogoImg} alt="로고이미지" />
-          </Link>
-        </h1>
-
-        <div className="login-type-wrapper">
-          <LoginBtnStyle value="BUYER" onClick={handleLoginType} clickChange={signUpType === 'BUYER'}>
-            구매회원 회원가입
-          </LoginBtnStyle>
-          <LoginBtnStyle value="SELLER" onClick={handleLoginType} clickChange={signUpType === 'SELLER'}>
-            판매회원 회원가입
-          </LoginBtnStyle>
-        </div>
-
-        <SignUpForm signUpType={signUpType} setSuccessUserName={setSuccessUserName} />
-      </SSignUpContainer>
+      <FormContainer setSuccessUserName={setSuccessUserName} />
       {successUserName !== '' && <Modal type={'successSignUp'} successUserName={successUserName} />}
     </>
   );
 }
-
-const SSignUpContainer = styled.div`
-  margin: 100px auto;
-  max-width: 550px;
-  text-align: center;
-
-  h1 img {
-    max-width: 238px;
-    margin-bottom: 70px;
-  }
-
-  .join-find-wrapper {
-    margin-top: 30px;
-    color: #333;
-    font-weight: 400;
-    font-size: 16px;
-  }
-`;
-
-type ClickChangeType = {
-  clickChange: boolean;
-};
-
-const LoginBtnStyle = styled.button`
-  margin-bottom: -10px;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 22px;
-  width: 275px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  border: 1px solid #c4c4c4;
-  border-bottom: ${(props: ClickChangeType) => (props.clickChange ? 'none' : '')};
-  background-color: ${(props: ClickChangeType) => (props.clickChange ? '#fff' : '#F2F2F2')};
-  padding: 20px;
-`;
